2dc029964a50a00c8117e5ef6591ce8c26d652db
harshpro <67826139+harshpro@users.noreply.github.com>
2020-10-30 08:19:11 +0530

    Add files via upload

---
+++ yoga.sql
@@ -0,0 +1,44 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 29, 2020 at 06:43 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `yoga`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `registrants`
+--
+
+CREATE TABLE `registrants` (
+  `fname` varchar(50) NOT NULL DEFAULT '',
+  `lname` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `city` varchar(50) NOT NULL,
+  `mobile` int(50) NOT NULL,
+  `booking_time` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
